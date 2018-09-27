8945fa41f3a8285e6261777af454e76fa0188c1e
clericus <k.s.pisarenko@gmail.com>
2018-09-27 14:00:27 +0300

    no comment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let balanceTrue = 0;                                                         
+  for(let i=0;i<bracketsConfig.length;i++){
+    let arr=bracketsConfig[i];                                  //pair array  
+    if(str.split(arr[0]).length === str.split(arr[1]).length){  //is brackets balanced
+      balanceTrue+=1;
+    }
+  }
+  let leng = str.length;                                          //length of string
+  for (let j = 0; j < leng*bracketsConfig.length; j++){           //parsing of string on pairs
+    for (let k = 0; k < bracketsConfig.length; k++){
+      let arr2=bracketsConfig[k];
+      str=str.replace(arr2[0]+arr2[1],'');                        //string will empty if brackets not cross   
+    }
+  }
+  if(balanceTrue===bracketsConfig.length&&str.length===0){       //is brackets balanced and not cross
+        return true;
+    }
+    else{
+        return false;
+        }
+  
 }
